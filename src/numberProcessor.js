a00cae5f4fee68dbaec6c0042f09ee2b95edde9b
WinterWyrm12 <exlander30@gmail.com>
2025-11-04 00:00:02 -0500

    added unit tests for numberProcessor.js

---
+++ src/numberProcessor.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+/* Sum */
+function sum(x){
+    let sum = 0;
+    for(let num of x){
+        sum += num;
+    } 
+    return sum;
+}
+
+/* Highest */
+function highest(x){
+    return Math.max(...x);
+}
+
+/* Lowest */
+function lowest(x){
+    return Math.min(...x);
+}
+
+/* Average */
+function average(x){
+    return sum(x)/x.length;
+}
+
+const numberstext = fs.readFileSync('C:/College Things/Rize_Application Development/Week 3/Assignment-2/data/sample-numbers.txt', 'utf8');
+const numbers = numberstext
+    .split('\n')
+    .map(line => line.trim())
+    .filter(line => line !== '')
+    .map(Number);
+
+/* Test */
+console.log('sum of all numbers: ', sum(numbers));
+console.log('highest number: ', highest(numbers));
+console.log('lowest number: ', lowest(numbers));
+console.log('average: ', average(numbers));
---
+++ src/textAnalyzer.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+/* Word Count */
+function wordCount(text){
+    // use regex to find words
+    const words = text.match(/[A-Za-z0-9']+/g);
+    return words.length;
+}
+
+/* Longest Word */
+function longestWord(text){
+    const words = text.match(/[A-Za-z0-9']+/g);
+    let longest = words[0];
+    for(let i of words){
+        if(i.length > longest.length){
+            longest = i;
+        }
+    }
+    return longest;
+}
+
+/* Line Count */
+function lineCount(text){
+    let count = text.split('\n').length;
+    return count;
+}
+
+/* Test */
+const sample = fs.readFileSync('C:/College Things/Rize_Application Development/Week 3/Assignment-2/data/sample-text.txt', 'utf8');
+const quotes = fs.readFileSync('C:/College Things/Rize_Application Development/Week 3/Assignment-2/data/quotes.txt', 'utf8');
+
+console.log("word count: ", wordCount(sample));
+console.log('Longest: ',longestWord(sample));
+console.log('Lines: ',lineCount(quotes));
